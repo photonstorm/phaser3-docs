27acc1a905dce6106b3151c4835af1997f40bad4
Richard Davey <rdavey@gmail.com>
2017-10-04 02:13:20 +0100

    Working through db structure

--- src/config.js
+++ src/config.js
@@ -3,6 +3,9 @@ module.exports = {
     SRC: '../phaser/v3/src/gameobjects/GameObject.js',
     DEST: './docs/comments.json',
 
+    SRC_PATH: '../phaser/v3/src/',
+    DEST_PATH: './docs/',
+
     DOCTRINE: {
         unwrap: true,
         recoverable: true,
--- src/index.js
+++ src/index.js
@@ -4,39 +4,6 @@ var beautify = require('json-beautify');
 var doctrine = require('doctrine');
 
 var CONFIG = require('./config');
+var ScanFile = require('./scanfile');
 
-fs.readFile(CONFIG.SRC, 'utf8', (err, data) => {
-
-    if (err)
-    {
-        throw err;
-    }
-
-    var comments = [];
-    var blocks = extract.block(data);
-
-    for (var i = 0; i < blocks.length; i++)
-    {
-        var block = blocks[i];
-
-        comments.push(doctrine.parse(block.value, CONFIG.DOCTRINE));
-    }
-
-    // comments = JSON.stringify(comments);
-    comments = beautify(comments, null, 2, 100); // just for debugging really
-    // comments = beautify(blocks, null, 2, 100); // just for debugging really
-
-    fs.writeFile(CONFIG.DEST, comments, { encoding: 'utf8', flag: 'w' }, function (error) {
-
-        if (error)
-        {
-            throw error;
-        }
-        else
-        {
-            console.log('Comments written');
-        }
-
-    });
-
-});
+ScanFile(CONFIG.SRC, 'GameObject.json');
