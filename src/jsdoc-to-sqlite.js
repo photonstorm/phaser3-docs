677489c70e0f854f43ddafbdecd452bede7052d2
Richard Davey <rich@photonstorm.com>
2018-02-21 17:14:26 +0000

    Working on return types.

--- src/jsdoc-to-sqlite.js
+++ src/jsdoc-to-sqlite.js
@@ -31,6 +31,97 @@ var getPath = function (path)
     return section.replace(/\\/g, '/');
 };
 
+var insertFunction = function (block, queries)
+{
+    var funcName = escape(block.longname);
+
+    var query = 'INSERT INTO functions VALUES (';
+
+    query = query.concat('"' + funcName + '",');
+    query = query.concat('"' + escape(block.since) + '",');
+    query = query.concat('"' + escape(block.name) + '",');
+    query = query.concat('"' + escape(block.memberof) + '",');
+    query = query.concat('"' + escape(block.description) + '",');
+    query = query.concat('"' + escape(block.scope) + '",');
+
+    //  Returns
+    var returns = 0;
+    var returnType = '';
+    var returnDescription = '';
+
+    if (Array.isArray(block.returns) && block.returns.length > 0)
+    {
+        //  There could be multiple return types (like object|null)
+        returns = 1;
+
+
+    }
+
+/*
+      "returns": [
+        {
+          "type": {
+            "names": [
+              "array"
+            ],
+            "parsedType": {
+              "type": "NameExpression",
+              "name": "array"
+            }
+          },
+          "description": "The array of Game Objects that was passed to this Action."
+        }
+      ],
+
+*/
+
+
+
+    //  Meta
+    query = query.concat('"' + escape(block.meta.filename) + '",');
+    query = query.concat(escape(block.meta.lineno) + ',');
+    query = query.concat('"' + escape(getPath(block.meta.path)) + '"');
+
+    query = query.concat(')');
+
+    queries.push(query);
+
+    if (Array.isArray(block.params) && block.params.length > 0)
+    {
+        for (var i = 0; i < block.params.length; i++)
+        {
+            var param = block.params[i];
+
+            var types = param.type.names.join('|');
+            var optional = -1;
+
+            if (param.hasOwnProperty('optional'))
+            {
+                optional = (param.optional) ? 1 : 0;
+            }
+
+            var defaultValue = (param.hasOwnProperty('defaultvalue')) ? param.defaultvalue : '';
+
+            query = 'INSERT INTO params VALUES (';
+
+            query = query.concat('"' + funcName + '",');
+            query = query.concat('"' + escape(param.name) + '",');
+            query = query.concat('"' + escape(param.description) + '",');
+            query = query.concat('"' + types + '",');
+            query = query.concat('"' + optional + '",');
+            query = query.concat('"' + defaultValue + '"');
+
+            query = query.concat(')');
+
+            queries.push(query);
+        }
+    }
+
+    console.log(funcName);
+
+    return query;
+};
+
 var insertClass = function (block, queries)
 {
     var className = escape(block.longname);
@@ -68,53 +159,6 @@ var insertClass = function (block, queries)
     {
         for (var i = 0; i < block.params.length; i++)
         {
-/*
-        //  Single type
-        {
-          "type": {
-            "names": [
-              "integer"
-            ],
-            "parsedType": {
-              "type": "NameExpression",
-              "name": "integer"
-            }
-          },
-          "optional": true,
-          "defaultvalue": 0,
-          "description": "Skip a number of frames. Useful when there are multiple sprite sheets in one Texture.",
-          "name": "config.startFrame"
-        },
-
-        //  Multi type
-        * @param {Phaser.Geom.Point|object} [out] - An object to store the return values in. If not given a Point object will be created.
-
-        {
-          "type": {
-            "names": [
-              "Phaser.Geom.Point",
-              "object"
-            ],
-            "parsedType": {
-              "type": "TypeUnion",
-              "elements": [
-                {
-                  "type": "NameExpression",
-                  "name": "Phaser.Geom.Point"
-                },
-                {
-                  "type": "NameExpression",
-                  "name": "object"
-                }
-              ]
-            }
-          },
-          "optional": true,
-          "description": "An object to store the return values in. If not given a Point object will be created.",
-          "name": "out"
-        }
-
- */
             var param = block.params[i];
 
             var types = param.type.names.join('|');
