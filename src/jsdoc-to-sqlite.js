f957bb95edb2773b270372d6a9833531631f797e
Richard Davey <rich@photonstorm.com>
2018-02-21 16:30:49 +0000

    Updated build script.

--- src/jsdoc-to-sqlite.js
+++ src/jsdoc-to-sqlite.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+var fs = require('fs-extra');
 var dirTree = require('directory-tree');
 var SQLite3 = require('better-sqlite3');
 
@@ -9,49 +9,160 @@ fs.copySync('./db/phaser-structure.db', './db/phaser-working.db');
 var db = new SQLite3('./db/phaser-working.db');
 
 //  Open the JSON file to parse
-var data = JSON.parse(require('./json/phaser.json'));
+var data = fs.readJsonSync('./json/phaser.json');
 
-console.log('hello?');
+//  For our first test let's parse classes only
 
+console.log('Scanning for classes ...');
+
+var getPath = function (path)
+{
+    // "D:\\wamp\\www\\phaser\\src\\gameobjects\\zone"
+
+    var i = path.indexOf('src');
+
+    if (i === -1)
+    {
+        return path;
+    }
+
+    var section = path.substr(i + 4);
+
+    return section.replace(/\\/g, '/');
+};
+
+var insertClass = function (block, queries)
+{
+    var className = escape(block.longname);
+
+    var query = 'INSERT INTO class VALUES (';
+
+    query = query.concat('"' + className + '",');
+    query = query.concat('"' + escape(block.since) + '",');
+    query = query.concat('"' + escape(block.name) + '",');
+    query = query.concat('"' + escape(block.memberof) + '",');
+    query = query.concat('"' + escape(block.classdesc) + '",');
+    query = query.concat('"' + escape(block.meta.filename) + '",');
+    query = query.concat(escape(block.meta.lineno) + ',');
+    query = query.concat('"' + escape(getPath(block.meta.path)) + '"');
+
+    query = query.concat(')');
+
+    queries.push(query);
+
+    //  Augments
+
+    if (Array.isArray(block.augments) && block.augments.length > 0)
+    {
+        for (var i = 0; i < block.augments.length; i++)
+        {
+            query = 'INSERT INTO extends VALUES ("' + className + '", "' + escape(block.augments[i]) + '")';
+
+            queries.push(query);
+        }
+    }
+
+    //  Constructor Params
+
+    if (Array.isArray(block.params) && block.params.length > 0)
+    {
+        for (var i = 0; i < block.params.length; i++)
+        {
 /*
+        //  Single type
+        {
+          "type": {
+            "names": [
+              "integer"
+            ],
+            "parsedType": {
+              "type": "NameExpression",
+              "name": "integer"
+            }
+          },
+          "optional": true,
+          "defaultvalue": 0,
+          "description": "Skip a number of frames. Useful when there are multiple sprite sheets in one Texture.",
+          "name": "config.startFrame"
+        },
+
+        //  Multi type
+        * @param {Phaser.Geom.Point|object} [out] - An object to store the return values in. If not given a Point object will be created.
+
+        {
+          "type": {
+            "names": [
+              "Phaser.Geom.Point",
+              "object"
+            ],
+            "parsedType": {
+              "type": "TypeUnion",
+              "elements": [
+                {
+                  "type": "NameExpression",
+                  "name": "Phaser.Geom.Point"
+                },
+                {
+                  "type": "NameExpression",
+                  "name": "object"
+                }
+              ]
+            }
+          },
+          "optional": true,
+          "description": "An object to store the return values in. If not given a Point object will be created.",
+          "name": "out"
+        }
+
+ */
+            var param = block.params[i];
+
+            var types = param.type.names.join('|');
+            var optional = -1;
+
+            if (param.hasOwnProperty('optional'))
+            {
+                optional = (param.optional) ? 1 : 0;
+            }
+
+            var defaultValue = (param.hasOwnProperty('defaultvalue')) ? param.defaultvalue : '';
+
+            query = 'INSERT INTO params VALUES (';
+
+            query = query.concat('"' + className + '",');
+            query = query.concat('"' + escape(param.name) + '",');
+            query = query.concat('"' + escape(param.description) + '",');
+            query = query.concat('"' + types + '",');
+            query = query.concat('"' + optional + '",');
+            query = query.concat('"' + defaultValue + '"');
+
+            query = query.concat(')');
+
+            queries.push(query);
+        }
+    }
+
+    console.log(className);
+
+    return query;
+};
+
 var queries = [];
 
-var filteredTree = dirTree(rootDir, { extensions: /\.js$/ }, (item, PATH) => {
+for (var i = 0; i < data.docs.length; i++)
+{
+    var block = data.docs[i];
 
-    item.path = item.path.replace('..\\phaser\\src\\', '');
-
-    if (
-        item.path.substr(-8) !== 'index.js' &&
-        item.path.substr(0, 21) !== 'physics\\matter-js\\lib' &&
-        item.path.substr(0, 9) !== 'polyfills'
-        )
+    if (block.kind === 'class')
     {
-        queries.push('INSERT INTO files (path) VALUES ("' + item.path + '")');
+        insertClass(block, queries);
     }
+}
 
-});
+console.log('Processing Queries: ', queries.length);
 
-filteredTree = beautify(filteredTree, null, 2, 100);
+db.transaction(queries).run();
 
-//  Save the JSON
+console.log('Complete.');
 
-fs.writeFile(outputJSON, filteredTree, function (error) {
-
-    if (error)
-    {
-        throw error;
-    }
-    else
-    {
-        console.log('files.json saved');
-        console.log('Running transaction (' + queries.length + ' queries)');
-
-        db.transaction(queries).run();
-
-        console.log('Complete. Now run \'sync\'');
-
-        db.close();
-    }
-
-});
-*/
+db.close();
