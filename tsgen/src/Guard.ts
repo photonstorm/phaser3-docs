c7a2bb917c5570b555b4a6255a5745468e16c9b5
Gareth Jones <jones258@gmail.com>
2019-02-19 10:11:17 +1300

    Create `Guard` class

---
+++ tsgen/src/Guard.ts
@@ -0,0 +1,17 @@
+import * as dom from 'dts-dom';
+
+export default class Guard {
+    /**
+     * Guards for types from `jsdoc` doclets.
+     */
+    static doclet = class {
+
+    };
+
+    /**
+     * Guards for types from `dts-dom`.
+     */
+    static dom = class {
+
+    };
+}
