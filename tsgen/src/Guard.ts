60f47c90d2a7e6f1ba0ba321f40cb5bacae7a48e
Gareth Jones <jones258@gmail.com>
2019-02-19 10:17:14 +1300

    Create `Guard.dom#isParameter` guard method

--- tsgen/src/Guard.ts
+++ tsgen/src/Guard.ts
@@ -12,6 +12,15 @@ export default class Guard {
      * Guards for types from `dts-dom`.
      */
     static dom = class {
-
+        /**
+         * Guards that the given `element` is of type `dom.Parameter`.
+         *
+         * @param {any | Parameter} element
+         *
+         * @return {element is Parameter}
+         */
+        static isParameter(element: any | dom.Parameter): element is dom.Parameter {
+            return element.kind === 'parameter';
+        }
     };
 }
