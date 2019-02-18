0131edc475bbc0f4df8bc866e8c563b080f7969b
Gareth Jones <jones258@gmail.com>
2019-02-19 10:20:40 +1300

    Create `Guard.doclet#isIDocletProp` guard method

--- tsgen/src/Guard.ts
+++ tsgen/src/Guard.ts
@@ -5,7 +5,21 @@ export default class Guard {
      * Guards for types from `jsdoc` doclets.
      */
     static doclet = class {
-
+        /**
+         * Guards that the given `doclet` is of type `IDocletProp`.
+         *
+         * @param {any | IDocletProp} doclet
+         *
+         * @return {doclet is IDocletProp}
+         */
+        static isIDocletProp(doclet: any | IDocletProp): doclet is IDocletProp {
+            return typeof doclet === 'object'
+                   && 'type' in doclet
+                   && 'name' in doclet
+                   && 'description' in doclet
+                   && 'comment' in doclet
+                   && typeof doclet.type === 'object';
+        }
     };
 
     /**
