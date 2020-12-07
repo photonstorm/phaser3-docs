0ba971a2b2413ef0f5231a617883afc708229e13
Francisco Pereira <gammafp@gmail.com>
2020-12-07 13:33:43 +0100

    Project cleaned and bootstrap added

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="antialiased">
+
+    @yield('content')
+
+</body>
+</html>
