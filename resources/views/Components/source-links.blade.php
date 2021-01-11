ab99825e9a538a9f415cc93a79e4c4c92074a716
Francisco Pereira Alvarado <gammafp@gmail.com>
2021-01-11 12:51:37 +0100

    Delete source-links.blade.php

--- resources/views/Components/source-links.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- Helps to get a card of sources link --}}
-<div {{ $attributes }}>Source:
-    <a href="https://github.com/photonstorm/phaser/blob/v{{config('app.phaser_version')}}/src/{{$metaFileRoute}}">src/{{$metaFileRoute}}</a>
-    <a href="https://github.com/photonstorm/phaser/blob/v{{config('app.phaser_version')}}/src/{{$metaFileRoute}}#L{{$metalineno}}">(Line {{$metalineno}})</a>
-
-</div>
