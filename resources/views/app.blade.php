78bbed8e989ed972c42a819eed1e4161cc571dee
Francisco Pereira <gammafp@gmail.com>
2021-03-12 17:34:44 +0100

    Google gtag addedx

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,11 +12,22 @@
 
     <!-- Adobe Fonts -->
     <script src="//use.typekit.net/fmc0gtt.js"></script>
-    <script>try{Typekit.load();}catch(e){}</script>
+    <script>
+        try{Typekit.load();}catch(e){}
+    </script>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YQ8ZV78C86"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-YQ8ZV78C86');
+    </script>
 </head>
 
 <body class="antialiased">
